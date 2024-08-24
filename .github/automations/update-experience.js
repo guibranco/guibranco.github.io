b7b65246d9c31db5a94ccd2eb3869e65e7412fd1
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2024-08-24 18:14:39 +0100

    Update update-experience.js

--- .github/automations/update-experience.js
+++ .github/automations/update-experience.js
@@ -2,7 +2,6 @@ const fs = require('fs');
 const chunk = require('chunk');
 const calculateAge = require('calculate-age').default;
 const fileName = 'experience.json';
-
 const columns = 5;
 
 const generateCell = (data) => `<td><strong>${calculateAge(data.since).getObject().years}+</strong> ${data.tech}</td>`;
@@ -18,23 +17,19 @@ const generateRow = (data) => {
 }
 
 async function main() {
-    try {
-        const data = fs.readFileSync(fileName, 'utf8');
-        const json = JSON.parse(data);
-        const rows = chunk(json, columns).map((row) => generateRow(row));
-        const html = `<table width="100%"><thead><th colspan="5">Experience (in years)</th></thead><tbody>${rows.join('')}</tbody></table>`;
+    const data = fs.readFileSync(fileName, 'utf8');
+    const json = JSON.parse(data);
+    const rows = chunk(json, columns).map((row) => generateRow(row));
+    const html = `<table width="100%"><thead><th colspan="5">Experience (in years)</th></thead><tbody>${rows.join('')}</tbody></table>`;
 
-        let experienceTable = '<!-- START EXPERIENCE TABLE -->';
-        experienceTable += html;
-        experienceTable += '<!-- END EXPERIENCE TABLE -->';
+    let experienceTable = '<!-- START EXPERIENCE TABLE -->';
+    experienceTable += html;
+    experienceTable += '<!-- END EXPERIENCE TABLE -->';
 
-        let indexHtml = fs.readFileSync('index.html', 'utf8');
-        indexHtml = indexHtml.replace(/<!-- START EXPERIENCE TABLE -->.*<!-- END EXPERIENCE TABLE -->/s, experienceTable);
+    let indexHtml = fs.readFileSync('index.html', 'utf8');
+    indexHtml = indexHtml.replace(/<!-- START EXPERIENCE TABLE -->.*<!-- END EXPERIENCE TABLE -->/s, experienceTable);
 
-        fs.writeFileSync('index.html', indexHtml, 'utf8');       
-    } catch (err) {
-        console.error(err);
-    }
+    fs.writeFileSync('index.html', indexHtml, 'utf8');
 }
 
 main();
