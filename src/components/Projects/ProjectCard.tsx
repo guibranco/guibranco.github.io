e3ea7ad4517f96440b52c840bd6238d649cdf208
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2025-03-19 10:56:32 +0000

    Update ProjectCard.tsx (#216)
    Co-authored-by: gstraccini[bot] <150967461+gstraccini[bot]@users.noreply.github.com>

--- src/components/Projects/ProjectCard.tsx
+++ src/components/Projects/ProjectCard.tsx
@@ -3,9 +3,10 @@ import React from "react";
 interface ProjectCardProps {
   name: string;
   repo: string;
+  showOwner: boolean;
 }
 
-const ProjectCard = ({ name, repo }: ProjectCardProps) => {
+const ProjectCard = ({ name, repo, showOwner }: ProjectCardProps) => {
   return (
     <a
       href={`https://github.com/${repo}`}
@@ -14,7 +15,7 @@ const ProjectCard = ({ name, repo }: ProjectCardProps) => {
       className="block"
     >
       <img
-        src={`https://github-readme-stats-git-feature-private-repositories-guibranco.vercel.app//api/pin/?username=${repo.split('/')[0]}&repo=${repo.split('/')[1]}&theme=synthwave&bg_color=341b3b&show_issues=true&show_pull_requests=true${repo.split('/')[0] !== 'guibranco' ? '&show_owner=true' : ''}`}
+        src={`https://github-readme-stats-git-feature-private-repositories-guibranco.vercel.app//api/pin/?username=${repo.split('/')[0]}&repo=${repo.split('/')[1]}&theme=synthwave&bg_color=341b3b&show_issues=true&show_pull_requests=true${repo.split('/')[0] !== 'guibranco' ? `&show_owner=${showOwner}` : ''}`}
         alt={name}
         className="w-full transition-transform duration-200 hover:scale-105"
       />
