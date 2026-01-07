4b6642d4014960493122db7f63458d2f83ad5acd
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2026-01-07 18:34:25 +0000

    Fix image source URL for PublicProjectCard (#606)
    Co-authored-by: gstraccini[bot] <150967461+gstraccini[bot]@users.noreply.github.com>

--- src/components/Projects/PublicProjectCard.tsx
+++ src/components/Projects/PublicProjectCard.tsx
@@ -8,7 +8,7 @@ const PublicProjectCard = ({ name, repo, showOwner }: PublicProjectCardProps) =>
   return (
     <a href={`https://github.com/${repo}`} target="_blank" rel="noopener" className="block">
       <img
-        src={`https://github-readme-stats-git-feature-private-repositories-guibranco.vercel.app//api/pin/?username=${repo.split("/")[0]}&repo=${repo.split("/")[1]}&theme=synthwave&bg_color=341b3b&show_issues=true&show_pull_requests=true${showOwner ? "&show_owner=true" : ""}`}
+        src={`https://github-readme-stats-guibranco.vercel.app/api/pin/?username=${repo.split("/")[0]}&repo=${repo.split("/")[1]}&theme=synthwave&bg_color=341b3b&show_issues=true&show_pull_requests=true${showOwner ? "&show_owner=true" : ""}`}
         alt={name}
         className="w-full transition-transform duration-200 hover:scale-105"
       />
